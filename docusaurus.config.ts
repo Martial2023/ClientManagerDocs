597b8ea560c4a9ac40ea7e762237db357fc3e09d
Martial Hilarion Avadra <martialhilarionavadra@gmail.com>
2025-09-24 14:47:11 +0200

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://github.com/Martial2023/',
+  baseUrl: '/ClientManagerDocs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Martial2023', // Usually your GitHub org/user name.
+  projectName: 'ClientManagerDocs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
